b3f27409e47f5fa16050633f0a902964f7c7d1ae
Robert Y <project@gmail.com>
2026-09-10 00:36:28 +0000

    Projected Completed

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,6 +24,8 @@ resource "aws_ecr_repository" "devops_app" {
   name                 = "devops-app"
   image_tag_mutability = "MUTABLE"
 
+  force_delete = true
+
   image_scanning_configuration {
     scan_on_push = false
   }
